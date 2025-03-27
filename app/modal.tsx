672ae36d310d05cc90c0bf6ce9722650ee1d5d10
Steven <scole.notredame23@gmail.com>
2025-03-27 00:11:46 -0600

    Modal and Login Base
    Modal implemented to prompt account sign in/up or skip
    -not connected to database (yet)

---
+++ app/modal.tsx
@@ -0,0 +1,20 @@
+import { StyleSheet, Text, View, TextInput } from 'react-native';
+import  LogIn  from '../components/LogIn'
+
+export default function Modal() {
+    return (
+        <View style={styles.container}>
+        
+            <LogIn />
+        </View>
+        
+    );
+}
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        alignItems: 'center',
+        justifyContent: 'center',
+    },
+});
